32a06941af26f249ba65b6a81637c927abef2070
Zi Li Tan <ztan0050@student.monash.edu>
2022-10-08 13:25:13 +1100

    skeleton code for receiving from button

--- flask things/pages.py
+++ flask things/pages.py
@@ -1,7 +1,15 @@
 import common
+from flask import request
 
 app = common.app
 
 @app.route('/')
 def root():
     return 'hello'
+
+# receive stuff from button, save to server
+@app.route('/button_input', methods=['POST'])
+def receive_from_button():
+    # get input fields from button
+    # post onto server
+    pass
