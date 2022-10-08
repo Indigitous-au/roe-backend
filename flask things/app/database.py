d03f00cbc4d0e7ff53f558672430e61dfda706df
Zi Li Tan <ztan0050@student.monash.edu>
2022-10-09 00:44:46 +1100

    filled somestuff in database and parser

--- flask things/app/database.py
+++ flask things/app/database.py
@@ -1,3 +1,5 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 
 class ReportItem:
     def __init__(self, time=None, platform=None, screenshot=None, username=None, suspect=None, ip=None):
@@ -8,6 +10,27 @@ class ReportItem:
         self.suspect = suspect
         self.ip = ip
 
-#TODO put the data into the database
+hasura_uri = "http://hack2022.drwaryaa.com/v1/graphql"
+
+#TODO put the data into the database - this still isn't working
 def to_database(data: ReportItem):
+    # get client
+    transport = AIOHTTPTransport(url=hasura_uri)
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    # construct the query
+    query = gql(
+        """
+        mutation insertreport {
+            insert_report_one(object: {ip_address: $ip, platform: $platform, screenshot: $screenshot, username_report: $report, username_suspect: $suspect}) {
+                ip_address
+                platform
+                screenshot
+                username_report
+                username_suspect
+            }
+        }
+        """
+    )
+    variables = {"ip": data.ip, "platform":data.platform, "screenshot":data.screenshot, "report":data.username, "suspect":data.suspect}
+    client.execute(query, variable_values=variables)
     pass
